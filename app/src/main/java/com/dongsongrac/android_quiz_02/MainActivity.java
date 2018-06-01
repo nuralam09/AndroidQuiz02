cef12ff46f7283624b7e246a3303a20220e5a481
DSR <dongsongrac@gmail.com>
2018-06-01 07:58:19 +0700

    upload code

---
+++ app/src/main/java/com/dongsongrac/android_quiz_02/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dongsongrac.android_quiz_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    // for sign up
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
